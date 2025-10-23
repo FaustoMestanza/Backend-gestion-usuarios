c534a58db15c373b1db0097186103f03e79ff207
FaustoMestanza <134259861+FaustoMestanza@users.noreply.github.com>
2025-10-22 21:47:40 -0500

    Actualización microservicio usuarios con CRUD

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,14 @@
-from django.urls import path
-from .views import LoginView, RefreshView, MeView
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import LoginView, RefreshView, MeView, UserViewSet
+
+# 🔹 Router para el CRUD de usuarios
+router = DefaultRouter()
+router.register(r'usuarios', UserViewSet, basename='usuario')
 
 urlpatterns = [
     path("auth/login/",   LoginView.as_view(),   name="auth-login"),
     path("auth/refresh/", RefreshView.as_view(), name="auth-refresh"),
     path("auth/me/",      MeView.as_view(),      name="auth-me"),
+    path("", include(router.urls)),  
 ]
--- users/views.py
+++ users/views.py
@@ -1,22 +1,32 @@
-from django.shortcuts import render
-
-# Create your views here.
-
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from .serializers import LoginSerializer, UserSerializer
+from rest_framework import viewsets, permissions
+from .models import User  
 
 class LoginView(TokenObtainPairView):
-    permission_classes = [AllowAny]        # login es público
+    permission_classes = [AllowAny]
     serializer_class = LoginSerializer
 
 class RefreshView(TokenRefreshView):
-    permission_classes = [AllowAny]        # refresh es público
+    permission_classes = [AllowAny]
 
 class MeView(APIView):
-    permission_classes = [IsAuthenticated] # requiere Bearer access
+    permission_classes = [IsAuthenticated]
 
     def get(self, request):
         return Response(UserSerializer(request.user).data)
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all().order_by('id')
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        rol = self.request.query_params.get('rol')
+        if rol:
+            queryset = queryset.filter(rol__nombre__iexact=rol)  # 👈 adaptado a tu modelo
+        return queryset
